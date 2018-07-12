415c883eaff0d39f6aa6633f9d4bfbd10d6292bd
Rohan Thomas (Home) <rohanthomas@bigpond.com>
2018-07-12 23:44:11 +1000

    Added new database base animals functionality

---
+++ animalsrepository.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'connection.php';
+
+//$sql = "SELECT id, source, easting, northing, latitude, longitude, size, density, health, burnt, comment, creation_date, last_updated_date FROM blackberry";
+$sql = "SELECT id, category, family, genus, species, common_name, native, frequency, newsletter, source FROM animal";
+
+$result = $conn->query($sql);
+
+if ($result)
+{
+    //echo "OK";
+}
+else
+{
+    die(mysqli_error($conn));
+}
+$conn->close();
+
+?>

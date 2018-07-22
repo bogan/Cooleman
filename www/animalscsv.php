d1064b1067eaa56fd91fa5a24fec95dac6fbfebd
Rohan Thomas (Home) <rohanthomas@bigpond.com>
2018-07-22 23:02:37 +1000

    Added CSV export functionality for Plants and Animals

---
+++ www/animalscsv.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'animalsrepository.php';
+
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=animals.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('id', 'category', 'sub_category', 'family', 'genus', 'species', 'common_name', 'native', 'frequency', 'newsletter', 'source'));
+
+// loop over the rows, outputting them
+while ($row = $result->fetch_assoc()) fputcsv($output, $row);
+
+
+// echo("<p/>");
+
+// echo("<table border='1' cellpadding='0' cellspacing='0'>");
+// echo("<tr><th>ID</th><th>Easting</th><th>Northing</th><th>Latitude</th><th>Longitude</th></tr></tr>");
+
+// $myfile = fopen("blackberrys.csv", "r") or die("unable to open file!");
+// // output one line until end-of-file
+// while(!feof($myfile)) {
+// // $line = fgets($myfile);
+// printBlackberry(fgets($myfile));
+// }
+// fclose($myfile);
+
+// echo("</table>");
+
+// echo("<p/>");
+
+//echo("<table border='1' cellpadding='0' cellspacing='0'>");
+//echo("<tr><th>Variable</th><th>Value</th></tr>");
+//$point = redfearnGridtoLLDebug(0685921,0685921);
+//echo("</table>");
+//echo("<p/>");

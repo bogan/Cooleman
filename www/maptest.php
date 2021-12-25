3e7a4a589e27dc4b68b38efd0f01c5dd56682353
Rohan Thomas <rohan.thomas@ewater.org.au>
2021-12-26 09:31:01 +1100

    Deleted old website files

--- www/maptest.php
+++
@@ -1,4 +0,0 @@
-<?php $title = 'Test Map';	?>
-<?php include 'header.php'; ?>
-
-<iframe src="https://www.google.com/maps/d/embed?mid=1Yuo-h9YN4niHzF8BQ1L1pxT-hTqav0zJ" width="640" height="480"></iframe>

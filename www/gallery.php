e62dd6f76a58941885715839c7dd6909e7224f42
Rohan Thomas <rohan.thomas@ewater.org.au>
2021-12-29 18:35:41 +1100

    Fixed broken links

--- www/gallery.php
+++ www/gallery.php
@@ -28,103 +28,103 @@
     <!-- The slideshow -->
     <div class="carousel-inner" role="listbox">
         <div class="carousel-item active" style="background-image: url('images/rainbow.jpg')">
-            <img src="images/rainbow.jpg" alt="buh">
+            <img src="/images/rainbow.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/bluewren.jpg')">
-            <img src="images/bluewren.jpg" alt="buh">
+            <img src="/images/bluewren.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape1.jpg')">
-            <img src="images/landscape1.jpg" alt="buh">
+            <img src="/images/landscape1.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/turtle-new.jpg')">
-            <img src="images/turtle-new.jpg" alt="buh">
+            <img src="/images/turtle-new.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape2.jpg')">
-            <img src="images/landscape2.jpg" alt="buh">
+            <img src="/images/landscape2.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/lizard2.jpg')">
-            <img src="images/lizard2.jpg" alt="buh">
+            <img src="/images/lizard2.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/landscape3.jpg')">
-            <img src="images/landscape3.jpg" alt="buh">
+            <img src="/images/landscape3.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/noisyminer.jpg')">
-            <img src="images/noisyminer.jpg" alt="buh">
+            <img src="/images/noisyminer.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape4.jpg')">
-            <img src="images/landscape4.jpg" alt="buh">
+            <img src="/images/landscape4.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/silvereye.jpg')">
-            <img src="images/silvereye.jpg" alt="buh">
+            <img src="/images/silvereye.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/currawong.jpg')">
-            <img src="images/currawong.jpg" alt="buh">
+            <img src="/images/currawong.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape6.jpg')">
-            <img src="images/landscape6.jpg" alt="Los Angeles">
+            <img src="/images/landscape6.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/woodduck3new.jpg')">
-            <img src="images/woodduck3new.jpg" alt="Los Angeles">
+            <img src="/images/woodduck3new.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape7.jpg')">
-            <img src="images/landscape7.jpg" alt="Los Angeles">
+            <img src="/images/landscape7.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/redneckedwallaby.jpg')">
-            <img src="images/redneckedwallaby.jpg" alt="Los Angeles">
+            <img src="/images/redneckedwallaby.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/landscape11.jpg')">
-            <img src="images/landscape11.jpg" alt="Los Angeles">
+            <img src="/images/landscape11.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/bird1.jpg')">
-            <img src="images/bird1.jpg" alt="Los Angeles">
+            <img src="/images/bird1.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape15.jpg')">
-            <img src="images/landscape15.jpg" alt="Los Angeles">
+            <img src="/images/landscape15.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/cockatoo.jpg')">
-            <img src="images/cockatoo.jpg" alt="Los Angeles">
+            <img src="/images/cockatoo.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape16.jpg')">
-            <img src="images/landscape16.jpg" alt="Los Angeles">
+            <img src="/images/landscape16.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/echidna1.jpg')">
-            <img src="images/echidna1.jpg" alt="Los Angeles">
+            <img src="/images/echidna1.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/landscape17.jpg')">
-            <img src="images/landscape17.jpg" alt="Los Angeles">
+            <img src="/images/landscape17.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/kangaroo1.jpg')">
-            <img src="images/kangaroo1.jpg" alt="Los Angeles">
+            <img src="/images/kangaroo1.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape18.jpg')">
-            <img src="images/landscape18.jpg" alt="Los Angeles">
+            <img src="/images/landscape18.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/kangaroos1.jpg')">
-            <img src="images/kangaroos1.jpg" alt="Los Angeles">
+            <img src="/images/kangaroos1.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/landscape.jpg')">
-            <img src="images/landscape.jpg" alt="Los Angeles">
+            <img src="/images/landscape.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/rosella1.jpg')">
-            <img src="images/rosella1.jpg" alt="Los Angeles">
+            <img src="/images/rosella1.jpg" alt="buh">
         </div>
 
         <div class="carousel-item" style="background-image: url('images/landscape13.jpg')">
-            <img src="images/landscape13.jpg" alt="Los Angeles">
+            <img src="/images/landscape13.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/rosella2.jpg')">
-            <img src="images/rosella2.jpg" alt="Los Angeles">
+            <img src="/images/rosella2.jpg" alt="buh">
         </div>
         <div class="carousel-item" style="background-image: url('images/rosella3.jpg')">
-            <img src="images/rosella3.jpg" alt="Los Angeles">
+            <img src="/images/rosella3.jpg" alt="buh">
         </div>
     </div>
 
